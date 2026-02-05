9eeb007da0bd950ce30e50f9a178de86ade9a6cc
Kris465 <nataza7394@gmail.com>
2026-02-05 20:18:03 +0300

    some some changes

--- model.py
+++ model.py
@@ -1,12 +1,17 @@
-class Database:
-    def __init__(self):
-        pass
+import sqlite3
 
-    def connections(self):
-        pass
+
+class Database:
+    def __init__(self, db_name="database.db"):
+        self.db_name = db_name
+        self.connection = None
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.db_name)
+        return self.connection
 
     def read_from_db(self):
         pass
 
-    def write_to_db(self):
+    def write_to_db(self, number):
         pass
